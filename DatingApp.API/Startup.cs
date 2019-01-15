af40f71722fddc423af799903e2df75e341d49a9
Christopher Culp <Christopher.D.Culp@gmail.com>
2019-01-15 15:09:22 -0500

    Completed v1, ready for production optimization

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -89,7 +89,14 @@ namespace DatingApp.API
 
             app.UseCors(X => X.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
-            app.UseMvc();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Fallback", action = "Index"}
+                );
+            });
         }
     }
 }
